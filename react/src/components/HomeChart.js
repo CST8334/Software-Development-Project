95552a4b6790bd6513d3cc549f487ea4a39689b5
Marko Nikic <nikicmarko14@gmail.com>
2021-05-27 14:45:05 -0400

    Added chart page

---
+++ react/src/components/HomeChart.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function HomeChart() {
+    return (
+        <div>
+            <h1>charts</h1>
+        </div>
+    )
+}
+
+export default HomeChart
--- react/src/pages/Home.js
+++ react/src/pages/Home.js
@@ -1,17 +1,23 @@
 import React from 'react'
 import Nav from '../components/Nav'
 import styled from 'styled-components';
+import HomeChart from '../components/HomeChart';
 
 function Home() {
     return (
         <Container>
             <Nav />
-            <h1>home</h1>
+            <h1>Here are the highlights of your account</h1>
+            <HomeChart />
         </Container>
     )
 }
 
 const Container = styled.div`
+    h1{
+        text-align: center;
+        margin-top: 70px;
+    }
 `
 
 export default Home
