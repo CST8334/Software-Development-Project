dede296661f9a9d2c39d3c0f95240a91ea2ce1a3
danstonmurphy <cumm0082@algonquinlive.com>
2021-05-29 12:54:59 -0400

    I thin i may have deleted nav by accedent

---
+++ react/src/components/WhatsNew.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { useLocation } from 'react-router';
+import styled from 'styled-components';
+
+function WhatsNew() {
+    const { pathname } = useLocation();
+    return (
+
+    )
+}
+
+const StyleWhatsNew = styled.whatsNew`
+    
+
+export default whatsNew
