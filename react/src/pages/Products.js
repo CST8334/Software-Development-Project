8cbe0a122929a1275bfa292a5ede04a9ea46f8ec
janiomjunior <mend0152@algonquinlive.com>
2021-06-04 11:57:44 -0400

    begining of product page

--- react/src/pages/Products.js
+++ react/src/pages/Products.js
@@ -1,11 +1,45 @@
-import React from 'react'
+import React from 'react';
+
+
 
 const Products = () => {
     return (
         <div>
             <h1>Products</h1>
+            <div>
+                <label for="compliance">Compliance Status</label>
+                <br/>
+                    <select id="compliance">
+                        <option value="volvo">Compliant</option>
+                        <option value="saab">Soon to Expire</option>
+                        <option value="opel">Expired</option>
+                    </select>
+            </div>    
+            <div>
+                <label for="country">Country</label>
+                <br/>
+                    <select id="country">
+                        <option value="volvo">United States</option>
+                        <option value="saab">Brazil</option>
+                        <option value="opel">Canada</option>
+                        <option value="opel">Mexico</option>
+                    </select>
+            </div>
+            <div>
+                <label for="body">Issuing Body</label>
+                <br/>
+                    <select id="body">
+                        <option value="volvo">Option 1</option>
+                        <option value="saab">Option 2</option>
+                        <option value="opel">Option 3</option>
+                        <option value="opel">Option 4</option>
+                    </select>
+            </div>
         </div>
+        
     )
 }
+
+
 
 export default Products
