00effb34b3fe4d1fdec702da7fba3e5b79cb55fe
janiomjunior <mend0152@algonquinlive.com>
2021-06-16 08:59:24 -0400

    more stuff products

--- react/src/pages/Products.js
+++ react/src/pages/Products.js
@@ -1,4 +1,3 @@
-
 import React, { useState } from 'react';
 import Toggle from '../components/Toggle';
 import styled from 'styled-components';
@@ -48,12 +47,10 @@ const Products = () => {
                         </Modal>
                 </div>
             </div> 
-            
         </FirstLine>
         
         <Main>
             <ProdSelec>
-                <div>
                         <div>
                             <label for="compliance">Compliance Status</label>
                             <br/>
@@ -78,18 +75,20 @@ const Products = () => {
                             <label for="body">Issuing Body</label>
                             <br/>
                                 <select id="body">
-                                    <option value="Option 1">c</option>
+                                    <option value="Option 1">Option 1</option>
                                     <option value="Option 2">Option 2</option>
                                     <option value="Option 3">Option 3</option>
                                     <option value="Option 4">Option 4</option>
                                 </select>
                         </div>
                         <CheckBox>
-                            <input type="checkbox" id="switch" /><label for="switch">Toggle</label>
-                        </CheckBox>
-                </div>
+                                <label class="switch">
+                                    <input type="checkbox"/>
+                                    <span class="slider round"></span>
+                                </label><label className="Switch">Show products whose documents have unread comments </label>
+                        </CheckBox>     
             </ProdSelec>
-                <div>
+            <Document>
                     <Faq>
                             <Toggle title="Company 1">
                                 <div className="anwser">
@@ -100,67 +99,89 @@ const Products = () => {
                                 <div className="anwser">
                                     <p>MODEL DOCUMENT - VERSION</p>
                                 </div>
-                            </Toggle>
-                            
+                            </Toggle>    
                     </Faq>
-                </div>
-                
+                    <button > LOAD MORE </button>           
+            </Document>
         </Main>
     </Container>
          
     )
 }
 
+
+const Document = styled.div`
+    flex-direction: column;
+
+`
+
 const CheckBox = styled.div`
-    input[type=checkbox]{
-        height: 0;
-        width: 0;
-        visibility: hidden;
-    }
-
-    label {
-        cursor: pointer;
-        text-indent: -9999px;
-        width: 20px;
-        height: 10px;
-        background: blue;
-        display: block;
-        border-radius: 10px;
-        position: relative;
-    }
-
-    label:after {
-        content: '';
-        position: absolute;
-        top: px;
-        left: 1px;
-        width: 9px;
-        height: 9px;
-        background: #fff;
-        border-radius: 9px;
-        transition: 0.3s;
-    }
-
-    input:checked + label {
-        background: grey;
-    }
-
-    input:checked + label:after {
-        left: calc(100% - 5px);
-        transform: translateX(-100%);
-    }
-
-    label:active:after {
-        width: 130px;
-    }
-
-    // centering
-    body {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        height: 100vh;
-    }
+.switch {
+    position: relative;
+    display: inline-block;
+    width: 50px;
+    height: 20px;
+  }
+  
+  .switch input { 
+    opacity: 0;
+    width: 0;
+    height: 0;
+  }
+  
+  .slider {
+    position: absolute;
+    cursor: pointer;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color: #ccc;
+    -webkit-transition: .4s;
+    transition: .4s;
+  }
+  
+  .slider:before {
+    position: absolute;
+    content: "";
+    height: 15px;
+    width: 15px;
+    left: 4px;
+    bottom: 4px;
+    background-color: white;
+    -webkit-transition: .4s;
+    transition: .4s;
+  }
+  
+  input:checked + .slider {
+    background-color: #2196F3;
+  }
+  
+  input:focus + .slider {
+    box-shadow: 0 0 1px #2196F3;
+  }
+  
+  input:checked + .slider:before {
+    -webkit-transform: translateX(26px);
+    -ms-transform: translateX(26px);
+    transform: translateX(26px);
+  }
+  
+  /* Rounded sliders */
+  .slider.round {
+    border-radius: 34px;
+  }
+  
+  .slider.round:before {
+    border-radius: 50%;
+  }
+   label {
+       font-size: 12px;
+   }
+   .Switch{
+       margin-left: 4px;
+       font-size: 12px;
+   }
 `
 
 const FirstLine = styled.div`
@@ -171,6 +192,10 @@ const FirstLine = styled.div`
     justify-content: space-evenly;
     align-items: center;
     flex-wrap: wrap;
+
+    button {
+        background-color: #ccc;
+    }
 `
 
 const Container = styled.div`
@@ -212,8 +237,12 @@ const ProdSelec = styled.div`
     background: white;
     flex-direction: column;
     border-radius: 20px;
-`
 
+    .Switch {
+        flex-direction: row;
+    }
+    
+`
 const Faq = styled.div`
 
     width: 600px;
