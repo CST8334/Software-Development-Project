dc4d22151f42fbd45427a5aa08252a4c0b4d57bb
danstonmurphy <cumm0082@algonquinlive.com>
2021-06-30 10:26:24 -0400

    June 30th made it prettyer

--- react/src/pages/Home.js
+++ react/src/pages/Home.js
@@ -2,7 +2,7 @@ import React from 'react'
 import styled from 'styled-components';
 import HomeChart from '../components/HomeChart';
 import TabNav from '../components/TabNav';
-
+//recat has setup test
 function Home() {
     return (
         <Container>
@@ -23,30 +23,37 @@ function Home() {
                             <h3>this is comments tab</h3>
                         </Tabcontent>
                     </Tabs>
+                    <img src= "react/src/img/SampleGraph.png"/> 
                 </Port>
                 <Starboard>
                     <label>What's New</label>
                     <NoticeBars>
-                        <label>Expiry notice</label>
+                        <label><h1>Expiry notice</h1></label>
                         <div>
+                        <p>
                         <Button>Secondary</Button>
-                        <a href="#">  Dismiss</a>
+                        <a href="#">Dismiss</a>
+                        </p>
                         </div>
                     </NoticeBars>
 
                     <NoticeBars>
                     <label>New stuff</label>
                         <div>
+                        <p>
                         <Button>Secondary</Button>
-                        <a href="#">  Dismiss</a>
+                        <a href="#">Dismiss</a>
+                        </p>
                         </div>
                     </NoticeBars>
 
                     <NoticeBars>
                     <label>Set a reminder</label>
                         <div>
+                        <p>
                         <Button>Secondary</Button>
-                        <a href="#">  Dismiss</a>
+                        <a href="#">Dismiss</a>
+                        </p>
                         </div>
                     </NoticeBars>
                 </Starboard>
@@ -85,18 +92,19 @@ const Main = styled.div`
     align-items: center;
     flex-wrap: wrap;
 `
+// Starboard means right
 const Starboard = styled.div`
-    width: 200px;
-    height: 200px;
+    width: 40%;
+    height: 400px;
     margin: 20px;
     padding: 1px;
     display: flex;
     flex-direction: column;
 `
-
+// Port means left
 const Port = styled.div`
-    width: 50%px;
-    height: 200px;
+    width: 40%;
+    height: 400px;
     margin: 20px;
     padding: 1px;
     display: flex;
@@ -109,11 +117,38 @@ const Button = styled.button`
 `
 
 const NoticeBars = styled.div`
-    border-radius: 5px;
+    height: 100px;
+    border-radius: 20px;
     background: white;
-    padding: 1px;
+    padding: 10px;
     margin: 10px;
+
+
+    button{
+        padding: 15px 32px;
+        background-color: white;
+        border-color: lightgrey;
+        border-size: 0.5px;
+        margin: 4px 2px;
+        box-shadow: 1px 2px #ccc;
+    }
+    button:hover {
+        background-color: #ccc;
+    }
+    a:href{
+        margin: 4px 2px;
+        font-size: 10px;
+        text-decoration: underline;
+        text-color: #56bbe7;
+        
+    }
 `
+//The margins that make the split section for the middle of the page
+
+// const Dismisser = style.a`
+//     text-size: 12px;
+
+// `
 
 const Tabs = styled.div`
     overflow: hidden;
@@ -147,6 +182,7 @@ const Tabs = styled.div`
         border: 1px solid red;
     }
 `
+//Stuff thats in the complience, product, and comments tabs
 const Tabcontent = styled.div`
     display: none;
     padding: 6px 12px;
